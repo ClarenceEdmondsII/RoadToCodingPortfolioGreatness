a2c2803c36b31c1251cfacd0241bc565a8d98de6
Clarence Edmonds II <clarenceedmondsii@clarences-mbp.mynetworksettings.com>
2024-10-24 18:14:33 -0400

    Added first project folder to repo. Contains a py file with instructions.

---
+++ Project1/workoutsummary.py
@@ -0,0 +1,10 @@
+'''Objective 1: Create an interactive prompt in a function that builds out a workout plan. Your prompt should ask for the workout name,
+muscle concentration (i.e. chest, shoulders, biceps, triceps, etc.), number of sets, number of reps, note/description.  Take
+all the responses to build a dictionary and return results to the function call. 
+'''
+
+def workout_prompt():
+    " code goes here"
+
+
+workout_prompt()
