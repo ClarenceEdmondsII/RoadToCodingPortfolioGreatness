2e62c4d1dea22f24853c8dc52d048c4e4bbad4b8
PhePhe123 <121632555+PhePhe123@users.noreply.github.com>
2024-12-11 10:41:30 -0500

    "Workout Summary Test"

--- Project1/workoutsummary.py
+++ Project1/workoutsummary.py
@@ -4,7 +4,16 @@ all the responses to build a dictionary and return results to the function call.
 '''
 
 def workout_prompt():
-    " code goes here"
+    workout_plan = {}
+    # plan_details = ["Workout Name", "Muscle Focus", "Number of Sets", "Number of Reps", "Notes/Description"]
+    workout_plan["Workout Name"] = input('What is the name of your workout?\n')
+    workout_plan["Muscle Concentration"] = input('What is the muscle focus of this plan?\n')
+    workout_plan["Number of sets"] = int(input('How many sets are in this workout?\n'))
+    workout_plan["Number of reps"] = int(input('How many reps are in this workout?\n'))
+    workout_plan["Description"] = input('Provide a brief description of this workout\n')
+
+    print(workout_plan)
+    return workout_plan
 
 
 workout_prompt()
